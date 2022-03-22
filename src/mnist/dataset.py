ff74f472fe86076394f3d7d022a8dadddf116dec
Iden Kalemaj <58532526+iden-kalemaj@users.noreply.github.com>
2022-03-22 16:48:32 -0400

    Update dataset.py

--- src/mnist/dataset.py
+++ src/mnist/dataset.py
@@ -9,7 +9,7 @@ def data_loaders(batch_size):
     test_data = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
     sampler = RandomSampler(train_data, replacement=True)
-    train_loader = DataLoader(train_data, batch_size, num_workers=8, sampler=sampler)
+    train_loader = DataLoader(train_data, batch_size, num_workers=2, sampler=sampler)
     test_loader = DataLoader(test_data, batch_size=1)
 
     return train_loader, test_loader
